3540a537c533b77c56d1361826b959bfaa6b92dc
irof <irof@javaring.info>
2012-04-30 16:35:59 +0900

    例外のテスト

--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,9 +1,13 @@
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
+import org.hamcrest.Description;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
+import org.junit.internal.matchers.TypeSafeMatcher;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 @RunWith(Enclosed.class)
@@ -90,8 +94,7 @@ public class VendingMachineTest {
 
 		@Test
 		public void 合計金額を表示する() throws Exception {
-			int actual = sut.getCurrent();
-			assertThat(actual, is(0));
+			assertThat(sut.getCurrent(), is(0));
 		}
 
 		@Test(expected = IllegalArgumentException.class)
@@ -106,8 +109,7 @@ public class VendingMachineTest {
 
 		@Test
 		public void 在庫数を表示する() throws Exception {
-			String actual = sut.getStock();
-			assertThat(actual, is("コーラ：5"));
+			assertThat(sut.getStock(), is("コーラ：5"));
 		}
 
 		@Test
@@ -118,6 +120,70 @@ public class VendingMachineTest {
 		@Test(expected = IllegalStateException.class)
 		public void 金額が足りないと買えない() throws Exception {
 			sut.sell("コーラ");
+		}
+	}
+
+	public static class ExceptionTest {
+		VendingMachine sut;
+
+		@Rule
+		public ExpectedException ex = ExpectedException.none();
+
+		@Before
+		public void setUp() {
+			sut = new VendingMachine();
+			sut.recieve(1000);
+		}
+
+		@Test
+		public void 変なものを突っ込まれたら例外_1() throws Exception {
+			try {
+				sut.recieve(123);
+				fail();
+			} catch (IllegalArgumentException e) {
+				assertThat(e.getMessage(), is("へんなもんいれんじゃね"));
+			}
+		}
+
+		@Test(expected = IllegalArgumentException.class)
+		public void 変なものを突っ込まれたら例外_2() throws Exception {
+			sut.recieve(123);
+		}
+
+		@Test
+		public void 変なものを突っ込まれたら例外_3() throws Exception {
+			ex.expect(IllegalArgumentException.class);
+			ex.expectMessage("へんなもんいれんじゃね");
+
+			sut.recieve(123);
+		}
+
+		@Test
+		public void 変なものを突っ込まれたら例外_4() throws Exception {
+			ex.expect(ほげ例外());
+
+			sut.recieve(123);
+		}
+
+		private TypeSafeMatcher<Exception> ほげ例外() {
+			return new TypeSafeMatcher<Exception>() {
+
+				@Override
+				public void describeTo(Description desc) {
+					desc.appendText("へんなもんいれんじゃね例外がでるはずだったのに…");
+				}
+
+				@Override
+				public boolean matchesSafely(Exception item) {
+					if (!(item instanceof IllegalArgumentException)) {
+						return false;
+					}
+					if (!item.getMessage().contains("へんなもんいれんじゃね")) {
+						return false;
+					}
+					return true;
+				}
+			};
 		}
 	}
 }
