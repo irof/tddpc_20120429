f2fc7ed843ea6f623d4b528dcbd0d57ed1be7286
irof <irof@javaring.info>
2012-04-30 14:26:08 +0900

    テストのリファクタリング

--- src/test/java/VenderMachineTest.java
+++ src/test/java/VenderMachineTest.java
@@ -1,17 +1,21 @@
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class VenderMachineTest {
 
+	VenderMachine sut;
+
+	@Before
+	public void setUp() {
+		sut = new VenderMachine();
+	}
+
 	@Test
 	public void 百円を受け取る() throws Exception {
-		VenderMachine sut = new VenderMachine();
-
-		// 百円をわたす
 		sut.recieve(100);
 
 		int actual = sut.total();
@@ -20,9 +24,6 @@ public class VenderMachineTest {
 
 	@Test
 	public void 百円を二枚受け取る() throws Exception {
-		VenderMachine sut = new VenderMachine();
-
-		// 百円をわたす
 		sut.recieve(100);
 		sut.recieve(100);
 
@@ -32,7 +33,6 @@ public class VenderMachineTest {
 
 	@Test
 	public void 合計金額を表示する() throws Exception {
-		VenderMachine sut = new VenderMachine();
 		int actual = sut.total();
 		assertThat(actual, is(0));
 	}
